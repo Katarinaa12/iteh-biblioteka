79d53a50f72416ee29c74fbe5ecae241ab515b3b
katarinaa12 <katarinadjordjic25@gmail.com>
2023-12-17 12:18:23 +0100

    Kreirane putanje za autentifikaciju i User resurs

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|max:255',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'password' => Hash::make($request->password),
+            'admin' => false
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['user' => new UserResource($user), 'token' => $token]);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only(['email', 'password']))) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+        $user = User::where('email', $request->email)->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['user' => new UserResource($user), 'token' => $token]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->noContent();
+    }
+    public function user(Request $request)
+    {
+        return response()->json(new UserResource($request->user()));
+    }
+}
